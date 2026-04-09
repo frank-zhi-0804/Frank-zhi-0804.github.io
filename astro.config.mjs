551f1957d35b0c129986914f16d5ae485892a8c3
frank-zhi-0804 <ridndpsnd@gmail.com>
2026-04-09 21:49:09 +0800

    更新内容

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -9,5 +10,5 @@ export default defineConfig({
   site: 'https://frank-zhi-0804.github.io',
   // 必须加 base: '/'，否则部署到 Pages 会路径错误！
   base: '/',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), vue()],
 });
